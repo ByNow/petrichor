15f3ea348dc96d17ab4e08d09b3146b75113a072
Bynow <1923184056@qq.com>
2020-07-09 23:23:09 +0800

    build

---
+++ src/main/java/cn/bynow/pertrichor/PertrichorApplication.java
@@ -0,0 +1,13 @@
+package cn.bynow.pertrichor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PertrichorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PertrichorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/bynow/pertrichor/PertrichorApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.bynow.pertrichor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PertrichorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
